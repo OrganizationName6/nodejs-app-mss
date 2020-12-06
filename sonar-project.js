ab3f2cfc3a1ae919061662563bcb70fee6b8b063
cryptocyrus69 <73691007+cryptocyrus69@users.noreply.github.com>
2020-12-06 22:22:55 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.239.48.77:9095/cyrus/',
+  serverUrl: 'http://3.237.37.158:9095/cyrus/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
